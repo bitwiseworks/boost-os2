c4c8db807b2196f19f7584e0dddee7de84a9bfdf
SilvanScherrer <silvan.scherrer@aroa.ch>
2026-01-29 12:51:25 +0100

    add some more define to os2.hpp

--- boost/config/platform/os2.hpp
+++ boost/config/platform/os2.hpp
@@ -9,16 +9,27 @@
 
 #define BOOST_PLATFORM "OS2"
 #define BOOST_HAS_DIRENT_H
+#define BOOST_HAS_NL_TYPES_H
 
 //
 // Threading API:
 #define BOOST_HAS_PTHREADS
+#define BOOST_HAS_PTHREAD_YIELD
 #define BOOST_HAS_NANOSLEEP
 #define BOOST_HAS_GETTIMEOFDAY
 #define BOOST_HAS_SIGACTION
 
 #define BOOST_HAS_UNISTD_H
 #define BOOST_HAS_STDINT_H
+
+//  Default defines for BOOST_SYMBOL_EXPORT and BOOST_SYMBOL_IMPORT
+//  If a compiler doesn't support __declspec(dllexport)/__declspec(dllimport),
+//  its boost/config/compiler/ file must define BOOST_SYMBOL_EXPORT and
+//  BOOST_SYMBOL_IMPORT
+#ifndef BOOST_SYMBOL_EXPORT
+#  define BOOST_HAS_DECLSPEC
+#  define BOOST_SYMBOL_EXPORT __declspec(dllexport)
+#endif
 
 // boilerplate code:
 #include <boost/config/detail/posix_features.hpp>
